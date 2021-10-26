552da98ff319291e507ca28c33090c0622d827c1
Little-Bro <tom.ericdelmas@gmail.com>
2021-10-26 22:42:05 +0100

    code cleanup

--- src/displayfunctions.js
+++ src/displayfunctions.js
@@ -1,3 +1,40 @@
+function displaySymbolsAndCursor() {
+    // displaying alterations
+  for (let i = 0; i < numLignes; i++) {
+    for (let j = 0; j < armure[0]; j++) {
+      if (armure[1] == 'bemol') {
+        bemol.resize(80, 0);
+        image(bemol, 60 + j*20, 100 + i * 200 + parseInt(bemolsHauteurs[Object.keys(bemolsHauteurs)[j]]));
+      } else {
+        displayDiese(80 + j*20, 150 + i * 200 + parseInt(diesesHauteurs[Object.keys(diesesHauteurs)[j]]));
+      }
+    }
+
+    /* displaying symbols at the 
+     * beginning of lines */
+    image(cle_sol, 5, 130 + i * 200);
+
+    // displaying chiffrage
+    if (chiffrage[0] == '4' && chiffrage[1] == '4') {
+      image(c, 60 + numAlterations * 20, 165 + i * 200);
+      c.resize(85, 0);
+    } else {
+      textSize(60);
+      textFont('Times New Roman');
+      text(chiffrage[0], armure[0]*20 + 90, 210 + i * 200);
+      text(chiffrage[1], armure[0]*20 + 90, 250 + i * 200);
+    } 
+
+    // displaying the lines
+    lignes[i].display();
+
+    // green / red cursor
+    if (lignes[i].isMouseInLigne()) {
+      lignes[i].checkMouse(lignes[i].isMouseColliding(notes));
+    }
+  }
+}
+
 function displayTempoNote(x, y) {
     push();
     translate(x, y);
@@ -22,4 +59,26 @@ function displayDiese(x, y) {
   line(-8, -5, 12, -5);
   line(-10, 5, 10, 5);
   pop();
+}
+
+function displayNameAndTempo() {
+  // displaying name of partoche
+  textSize(60);
+  text(partoche.nom, width / 2 - (partoche.nom.length * 10), 80);
+  textSize(20);
+  text(`par ${partoche.auteur}`, width / 2 - (partoche.auteur.length * 5), 110);
+
+  // displaying tempo
+  displayTempoNote(110, 120);
+  textSize(24);
+  text(`= ${partoche.tempo}`, 130, 125);
+}
+
+function displayNotes() {
+  // displaying the notes
+  if (notes.length != 0) {
+    for (note of notes) {
+      note.display();
+    }
+  }  
 }
